ad1c51a059085f6d94348109197d8b2aa2508e2c
Jarupum Chaleesombut <>
2023-07-18 20:44:28 +0700

    Initial example code

--- secret_photo/urls.py
+++ secret_photo/urls.py
@@ -3,7 +3,11 @@ from django.urls import path
 from secret_photo import views
 
 urlpatterns = [
-    path('', views.index),
+    path('secret-photo/tonhom', views.index_tonhom),
+    path('secret-photo/kim', views.index_kim),
+    path('secret-photo/best', views.index_best),
+    path('secret-photo/authenticate/', views.authen_function),
+    path('secret-photo/register_page', views.register, name='register')
     # path('test2', views.index2),
     # path('api/login', views.LoginApiView.as_view(), name='login')
 ]
